258bd3e17cfd15c619b15ccadc5a78b98184907d
Frans Santoso <fsantoso@liberty.edu>
2023-03-21 11:58:07 -0400

    added mock up quiz page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,6 @@
 // import logo from '../assets/logo.svg';
 import { useState } from 'react';
+import Quiz from './Quiz';
 import '../styles/App.css';
 
 const App = () => {
@@ -23,15 +24,16 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Trivia Game</h1>
+        <h1>{(quizCategory.length > 0 ? quizCategory + ' Trivia' : 'Trivia Game')}</h1>
       </header>
       <div className='App-body'>
         <div className='App-selection' hidden={selectionHidden} style={(!selectionHidden) ? {display: 'flex'} : {display: 'none'}}>
-          <button className='animals' type="submit" onClick={() => handleClick("animals")}>Animals</button>
-          <button className='history' type="submit" onClick={() => handleClick("history")}>History</button>
-          <button className='science' type="submit" onClick={() => handleClick("science")}>Science</button>
-          <button className='sports' type="submit" onClick={() => handleClick("sports")}>Sports</button>
+          <button className='animals' type="submit" onClick={() => handleClick("Animals")}>Animals</button>
+          <button className='history' type="submit" onClick={() => handleClick("History")}>History</button>
+          <button className='science' type="submit" onClick={() => handleClick("Science")}>Science</button>
+          <button className='sports' type="submit" onClick={() => handleClick("Sports")}>Sports</button>
         </div>
+        <Quiz category={quizCategory}/>
       </div>
     </div>
   );
