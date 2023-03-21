415cb869fbf0ac99e9dbdd872cbc35aea7ec8246
Frans Santoso <fsantoso@liberty.edu>
2023-03-21 11:40:40 -0400

    added basic functionality to buttons

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,20 +1,37 @@
 // import logo from '../assets/logo.svg';
+import { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
+  const [selectionHidden, setSelectionHidden] = useState(false);
+  const [quizCategory, setQuizCategory] = useState('');
 
+  const handleClick = (category) => {
+    console.log(category);
+    setQuizCategory(category);
+    toggleSelection();
+  }
 
-  
+  function toggleSelection() {
+    if (selectionHidden) {
+      setSelectionHidden(false);
+    } else {  
+      setSelectionHidden(true);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Trivia Game</h1>
       </header>
       <div className='App-body'>
-        <button className='animals' type="submit">Animals</button>
-        <button className='history' type="submit">History</button>
-        <button className='science' type="submit">Science</button>
-        <button className='sports' type="submit">Sports</button>
+        <div className='App-selection' hidden={selectionHidden} style={(!selectionHidden) ? {display: 'flex'} : {display: 'none'}}>
+          <button className='animals' type="submit" onClick={() => handleClick("animals")}>Animals</button>
+          <button className='history' type="submit" onClick={() => handleClick("history")}>History</button>
+          <button className='science' type="submit" onClick={() => handleClick("science")}>Science</button>
+          <button className='sports' type="submit" onClick={() => handleClick("sports")}>Sports</button>
+        </div>
       </div>
     </div>
   );
