952a9a90cdf28213671a22950d6f8006e57ed5e9
Frans Santoso <fsantoso@liberty.edu>
2023-03-21 12:49:08 -0400

    added basic questions

---
+++ src/assets/Questions.js
@@ -0,0 +1,18 @@
+export const AnimalQuestions = [  
+    ["What animal is known as the “King of the Jungle”?", "Lion", ["Tiger", "Elephant", "Gorilla"]],
+    ["Which bird is known for its ability to mimic human speech?", "Parrot", ["Pigeon", "Peacock", "Ostrich"]],
+    ["What is the only continent where the polar bear can be found in the wild?", "North America", ["Asia", "Europe", "Antarctica"]],
+    ["What is the largest mammal in the world?", "Blue Whale", ["African Elephant", "Hippopotamus", "Giraffe"]],
+    ["What type of animal is a Komodo dragon?", "Lizard", ["Snake", "Crocodile", "Alligator"]],
+    ["What is the fastest land animal in the world?", "Cheetah", ["Lion", "Tiger", "Leopard"]],
+    ["What is the tallest animal in the world?", "Giraffe", ["Elephant", "Hippopotamus", "Rhinoceros"]],
+    ["What is the smallest mammal in the world?", "Bumblebee Bat", ["Squirrel", "Mole", "Shrew"]],
+    ["What is the largest bird in the world?", "Ostrich", ["Emu", "Cassowary", "Penguin"]],
+    ["What is the most venomous animal in the world?", "Box Jellyfish", ["Black Widow Spider", "Deathstalker Scorpion", "Stonefish"]],
+    ["What is the slowest animal in the world?", "Sloth", ["Snail", "Slug", "Turtle"]],
+    ["What is the largest reptile in the world?", "Saltwater Crocodile", ["Nile Crocodile", "American Alligator", "Green Anaconda"]],
+    ["What animal is the only mammal that can fly?", "Bat", ["Flying Squirrel", "Sugar Glider", "Opossum"]],
+    ["What is the largest fish in the world?", "Whale Shark", ["Giant Squid", "Sperm Whale", "Bluefin Tuna"]],
+    ["What is the largest amphibian in the world?", "Chinese Giant Salamander", ["African Bullfrog", "Goliath Frog", "Hellbender"]]
+]
+
