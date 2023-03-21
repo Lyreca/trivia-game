3c30542ae0e82722c460cf57cfde570d2d08a464
Frans Santoso <fsantoso@liberty.edu>
2023-03-21 13:11:08 -0400

    fixed quiz crashing after final question

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -8,41 +8,55 @@ const Quiz = (props) => {
     const [answerPool, setAnswerPool] = useState([]);
 
     useEffect(() => {
-        if (props.questions.length > 0) {
-            let answerPool = [];
-            answerPool.push(props.questions[quizNumber][1]);
-            answerPool = shuffle(answerPool.concat(props.questions[quizNumber][2]));
+        if(quizNumber < props.questions.length) {
+            if (props.questions.length > 0) {
+                let answerPool = [];
+                answerPool.push(props.questions[quizNumber][1]);
+                answerPool = shuffle(answerPool.concat(props.questions[quizNumber][2]));
 
-            console.log(answerPool)
-            setAnswerPool(answerPool);
+                console.log(answerPool)
+                setAnswerPool(answerPool);
+            }
+
+            if (quizAnswer.length > 0) {
+                if (quizAnswer === props.questions[quizNumber][1]) {
+                    // alert('Correct!')
+                    setQuizScore(quizScore + 1);
+                }
+                else {
+                    // alert('Incorrect!')
+                }
+                setQuizNumber(quizNumber + 1);
+                setQuizAnswer('');
+            }
         }
-
-        if (quizAnswer.length > 0) {
-            if (quizAnswer === props.questions[quizNumber][1]) {
-                alert('Correct!')
-                setQuizScore(quizScore + 1);
-            }
-            else {
-                alert('Incorrect!')
-            }
-            setQuizNumber(quizNumber + 1);
-            setQuizAnswer('');
+        else {
+            setQuizNumber(quizNumber+1);
         }
         
     }, [quizNumber, props.questions, quizAnswer, quizScore]);
 
     return (
         <div>
-            <h2>Question {quizNumber+1}</h2>
-            <h3>{props.questions[quizNumber][0]}</h3>
-            <div className='quiz-answers'>
-                {answerPool.map((answer, index) => {
-                    return (
-                        <button key={"answerBox" + index} className='quiz-answer' type="submit" onClick={() => setQuizAnswer(answer)}>{answer}</button>
-                    )
-                })}
-            </div>
-            <h3>Score: {quizScore}</h3>
+            {
+            (quizNumber < props.questions.length) ?
+                <div className='App-questions'>
+                    <h2>Question {quizNumber+1}</h2>
+                    <h3>{props.questions[quizNumber][0]}</h3>
+                    <div className='App-answers'>
+                        {answerPool.map((answer, index) => {
+                            return (
+                                <button key={"answerBox" + index} className='quiz-answer' type="submit" onClick={() => setQuizAnswer(answer)}>{answer}</button>
+                            )
+                        })}
+                    </div>
+                    <h3>Score: {quizScore}</h3>
+                </div>
+                : 
+                <div className='App-final'>
+                    <h2>Final Score: {quizScore}</h2>
+                </div>
+            }
         </div>
     )
 
