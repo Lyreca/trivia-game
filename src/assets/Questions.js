28bde4f5f3b20dbe6685b7e06bda272801bc6406
Frans Santoso <fsantoso@liberty.edu>
2023-03-22 00:05:53 -0400

    added sports and science questions

--- src/assets/Questions.js
+++ src/assets/Questions.js
@@ -123,12 +123,260 @@ export const HistoryQuestions = [
         "England and France",
         ["Spain and Portugal", "Germany and Austria", "Italy and Greece"]
       ],
-]
+    ]
 
-export const ScienceQuestions = [
-
-]
+export const ScienceQuestions =  [
+    [
+      "What is the smallest unit of matter?",
+      "Atom",
+      ["Electron", "Molecule", "Proton"]
+    ],
+    [
+      "What is the largest planet in our solar system?",
+      "Jupiter",
+      ["Saturn", "Uranus", "Neptune"]
+    ],
+    [
+      "What is the process by which plants make their own food called?",
+      "Photosynthesis",
+      ["Respiration", "Fermentation", "Digestion"]
+    ],
+    [
+      "What is the name of the force that pulls objects towards the center of the Earth?",
+      "Gravity",
+      ["Magnetism", "Friction", "Inertia"]
+    ],
+    [
+      "What is the name of the closest star to Earth?",
+      "Sun",
+      ["Alpha Centauri", "Proxima Centauri", "Betelgeuse"]
+    ],
+    [
+      "What is the name of the layer of the Earth's atmosphere where most weather occurs?",
+      "Troposphere",
+      ["Stratosphere", "Mesosphere", "Thermosphere"]
+    ],
+    [
+      "What is the name of the force that resists motion between two surfaces in contact?",
+      "Friction",
+      ["Gravity", "Magnetism", "Inertia"]
+    ],
+    [
+      "What is the name of the process by which water changes from a liquid to a gas?",
+      "Evaporation",
+      ["Condensation", "Sublimation", "Freezing"]
+    ],
+    [
+      "What is the name of the process by which water changes from a gas to a liquid?",
+      "Condensation",
+      ["Evaporation", "Sublimation", "Melting"]
+    ],
+    [
+      "What is the name of the process by which a solid changes directly into a gas?",
+      "Sublimation",
+      ["Evaporation", "Condensation", "Freezing"]
+    ],
+    [
+      "What is the name of the process by which a gas changes directly into a solid?",
+      "Deposition",
+      ["Sublimation", "Condensation", "Melting"]
+    ],
+    [
+      "What is the name of the process by which a liquid changes into a solid?",
+      "Freezing",
+      ["Melting", "Evaporation", "Condensation"]
+    ],
+    [
+      "What is the name of the process by which a solid changes into a liquid?",
+      "Melting",
+      ["Freezing", "Evaporation", "Sublimation"]
+    ],
+    [
+      "What is the name of the force that opposes the motion of an object through a fluid?",
+      "Drag",
+      ["Thrust", "Lift", "Gravity"]
+    ],
+    [
+      "What is the name of the process by which energy is transferred through a material without any physical movement of the material itself?",
+      "Conduction",
+      ["Convection", "Radiation", "Reflection"]
+    ],
+    [
+      "What is the name of the process by which heat energy is transferred through the movement of fluids?",
+      "Convection",
+      ["Conduction", "Radiation", "Reflection"]
+    ],
+    [
+      "What is the name of the process by which energy is transferred through electromagnetic waves?",
+      "Radiation",
+      ["Conduction", "Convection", "Reflection"]
+    ],
+    [
+      "What is the name of the process by which light bounces off a surface?",
+      "Reflection",
+      ["Refraction", "Absorption", "Scattering"]
+    ],
+    [
+      "What is the name of the process by which light bends as it passes through a material?",
+      "Refraction",
+      ["Reflection", "Absorption", "Scattering"]
+    ],
+    [
+      "What is the name of the force that causes an object to float in a fluid?",
+      "Buoyancy",
+      ["Gravity", "Friction", "Inertia"]
+    ],
+    [
+      "What is the name of the process by which sound waves bounce off surfaces?",
+      "Echo",
+      ["Resonance", "Refraction", "Absorption"]
+    ],
+    [
+      "What is the name of the process by which sound waves are amplified by a vibrating object?",
+      "Resonance",
+      ["Echo", "Refraction", "Absorption"]
+    ],
+    [
+      "What is the name of the measure of the amount of matter in an object?",
+      "Mass",
+      ["Weight", "Density", "Volume"]
+    ],
+    [
+      "What is the name of the measure of the force of gravity on an object?",
+      "Weight",
+      ["Mass", "Density", "Volume"]
+    ],
+    [
+      "What is the name of the measure of the amount of space an object occupies?",
+      "Volume",
+      ["Mass", "Density", "Weight"]
+    ]
+];
 
 export const SportsQuestions = [
-    
-]
+    [
+      "What country has won the most soccer World Cups?",
+      "Brazil",
+      ["Germany", "Argentina", "Italy"]
+    ],
+    [
+      "What baseball player holds the record for the most career home runs?",
+      "Barry Bonds",
+      ["Hank Aaron", "Babe Ruth", "Alex Rodriguez"]
+    ],
+    [
+      "What boxer famously said 'float like a butterfly, sting like a bee'?",
+      "Muhammad Ali",
+      ["Mike Tyson", "George Foreman", "Floyd Mayweather Jr."]
+    ],
+    [
+      "What basketball player won six NBA championships with the Chicago Bulls in the 1990s?",
+      "Michael Jordan",
+      ["Kobe Bryant", "LeBron James", "Magic Johnson"]
+    ],
+    [
+      "What country hosted the 2014 Winter Olympics?",
+      "Russia",
+      ["Canada", "Norway", "South Korea"]
+    ],
+    [
+      "What golfer has won the most major championships in men's golf?",
+      "Jack Nicklaus",
+      ["Tiger Woods", "Arnold Palmer", "Phil Mickelson"]
+    ],
+    [
+      "What tennis player has won the most Grand Slam singles titles in women's tennis?",
+      "Margaret Court",
+      ["Serena Williams", "Steffi Graf", "Martina Navratilova"]
+    ],
+    [
+      "What football player holds the record for the most career passing yards?",
+      "Drew Brees",
+      ["Peyton Manning", "Tom Brady", "Brett Favre"]
+    ],
+    [
+      "What country has won the most Olympic gold medals?",
+      "United States",
+      ["China", "Russia", "Germany"]
+    ],
+    [
+      "What hockey player has scored the most career goals in the NHL?",
+      "Wayne Gretzky",
+      ["Mario Lemieux", "Jaromir Jagr", "Bobby Orr"]
+    ],
+    [
+      "What sprinter holds the world record in the men's 100 meter dash?",
+      "Usain Bolt",
+      ["Carl Lewis", "Maurice Greene", "Donovan Bailey"]
+    ],
+    [
+      "What NFL team has won the most Super Bowls?",
+      "Pittsburgh Steelers",
+      ["New England Patriots", "Dallas Cowboys", "San Francisco 49ers"]
+    ],
+    [
+      "What country won the 2018 FIFA World Cup?",
+      "France",
+      ["Croatia", "Belgium", "England"]
+    ],
+    [
+      "What baseball player holds the record for the most career hits?",
+      "Pete Rose",
+      ["Ichiro Suzuki", "Ty Cobb", "Hank Aaron"]
+    ],
+    [
+      "What boxer was known as 'The Greatest'?",
+      "Muhammad Ali",
+      ["Mike Tyson", "George Foreman", "Floyd Mayweather Jr."]
+    ],
+    [
+      "What basketball player holds the record for the most career points scored?",
+      "Kareem Abdul-Jabbar",
+      ["LeBron James", "Kobe Bryant", "Michael Jordan"]
+    ],
+    [
+      "What country has won the most Olympic medals overall?",
+      "United States",
+      ["China", "Russia", "Germany"]
+    ],
+    [
+      "What golfer has won the most career PGA Tour events?",
+      "Sam Snead",
+      ["Tiger Woods", "Arnold Palmer", "Jack Nicklaus"]
+    ],
+    [
+      "What tennis player has won the most Grand Slam singles titles in men's tennis?",
+      "Roger Federer",
+      ["Rafael Nadal", "Novak Djokovic", "Pete Sampras"]
+    ],
+    [
+      "What football player holds the record for the most career rushing yards?",
+      "Emmitt Smith",
+      ["Walter Payton", "Barry Sanders", "Jim Brown"]
+    ],
+    [
+      "What country hosted the 2016 Summer Olympics?",
+      "Brazil",
+      ["China", "Russia", "Japan"]
+    ],
+    [
+      "What hockey player has the most career assists in the NHL?",
+      "Wayne Gretzky",
+      ["Mark Messier", "Ron Francis", "Steve Yzerman"]
+    ],
+    [
+      "What sprinter holds the world record in the women's 100 meter dash?",
+      "Florence Griffith-Joyner",
+      ["Marion Jones", "Usain Bolt", "Carmelita Jeter"]
+    ],
+    [
+      "What NFL team has the most Super Bowl appearances?",
+      "New England Patriots",
+      ["Pittsburgh Steelers", "San Francisco 49ers", "Dallas Cowboys"]
+    ],
+    [
+      "What country won the most medals at the 2016 Summer Olympics?",
+      "United States",
+      ["China", "Great Britain", "Russia"]
+    ]
+  ];
