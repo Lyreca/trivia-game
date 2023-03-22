d6fcd79634113936153b73d79faf343f850d3249
Frans Santoso <fsantoso@liberty.edu>
2023-03-21 22:23:25 -0400

    added history questions

--- src/assets/Questions.js
+++ src/assets/Questions.js
@@ -1,8 +1,24 @@
 export const AnimalQuestions = [  
-    ["What animal is known as the “King of the Jungle”?", "Lion", ["Tiger", "Elephant", "Gorilla"]],
-    ["Which bird is known for its ability to mimic human speech?", "Parrot", ["Pigeon", "Peacock", "Ostrich"]],
-    ["What is the only continent where the polar bear can be found in the wild?", "North America", ["Asia", "Europe", "Antarctica"]],
-    ["What is the largest mammal in the world?", "Blue Whale", ["African Elephant", "Hippopotamus", "Giraffe"]],
+    [
+        "What animal is known as the “King of the Jungle”?", 
+        "Lion", 
+        ["Tiger", "Elephant", "Gorilla"]
+    ],
+    [
+        "Which bird is known for its ability to mimic human speech?", 
+        "Parrot", 
+        ["Pigeon", "Peacock", "Ostrich"]
+    ],
+    [
+        "What is the only continent where the polar bear can be found in the wild?", 
+        "North America", 
+        ["Asia", "Europe", "Antarctica"]
+    ],
+    [
+        "What is the largest mammal in the world?", 
+        "Blue Whale", 
+        ["African Elephant", "Hippopotamus", "Giraffe"]
+    ],
     ["What type of animal is a Komodo dragon?", "Lizard", ["Snake", "Crocodile", "Alligator"]],
     ["What is the fastest land animal in the world?", "Cheetah", ["Lion", "Tiger", "Leopard"]],
     ["What is the tallest animal in the world?", "Giraffe", ["Elephant", "Hippopotamus", "Rhinoceros"]],
@@ -16,3 +32,103 @@ export const AnimalQuestions = [
     ["What is the largest amphibian in the world?", "Chinese Giant Salamander", ["African Bullfrog", "Goliath Frog", "Hellbender"]]
 ]
 
+export const HistoryQuestions = [
+    [
+        "What famous battle marked the end of the Napoleonic Wars?",
+        "Battle of Waterloo",
+        ["Battle of Austerlitz", "Battle of Borodino", "Battle of Leipzig"]
+      ],
+      [
+        "What was the first civilization to develop in ancient Greece?",
+        "Minoan civilization",
+        ["Mycenaean civilization", "Athenian civilization", "Spartan civilization"]
+      ],
+      [
+        "Which Roman emperor is known for building a wall across northern England?",
+        "Hadrian",
+        ["Augustus", "Nero", "Caligula"]
+      ],
+      [
+        "Which country was the first to recognize the United States as an independent nation?",
+        "Morocco",
+        ["France", "Spain", "Netherlands"]
+      ],
+      [
+        "Which country did the United States purchase Alaska from?",
+        "Russia",
+        ["Canada", "Denmark", "Sweden"]
+      ],
+      [
+        "Who was the first African American to serve as a justice on the United States Supreme Court?",
+        "Thurgood Marshall",
+        ["Clarence Thomas", "Sonia Sotomayor", "Elena Kagan"]
+      ],
+      [
+        "What was the first successful English colony in North America?",
+        "Jamestown",
+        ["Plymouth", "Roanoke", "Massachusetts Bay"]
+      ],
+      [
+        "What event sparked World War I?",
+        "Assassination of Archduke Franz Ferdinand",
+        ["Invasion of Poland", "Bombing of Pearl Harbor", "Fall of the Berlin Wall"]
+      ],
+      [
+        "Who was the first president of the United States?",
+        "George Washington",
+        ["Thomas Jefferson", "John Adams", "Benjamin Franklin"]
+      ],
+      [
+        "What was the first country to send a man into space?",
+        "Soviet Union",
+        ["United States", "China", "France"]
+      ],
+      [
+        "What was the last battle of the American Civil War?",
+        "Battle of Palmito Ranch",
+        ["Battle of Gettysburg", "Battle of Antietam", "Battle of Vicksburg"]
+      ],
+      [
+        "Who wrote the Communist Manifesto?",
+        "Karl Marx",
+        ["Friedrich Engels", "Vladimir Lenin", "Leon Trotsky"]
+      ],
+      [
+        "Who was the first European explorer to reach India by sea?",
+        "Vasco da Gama",
+        ["Christopher Columbus", "Ferdinand Magellan", "James Cook"]
+      ],
+      [
+        "Which country was the first to declare war in World War II?",
+        "Germany",
+        ["France", "United Kingdom", "Soviet Union"]
+      ],
+      [
+        "What was the first permanent English settlement in the New World?",
+        "Jamestown",
+        ["Plymouth", "Roanoke", "Massachusetts Bay"]
+      ],
+      [
+        "Who was the first woman to win a Nobel Prize?",
+        "Marie Curie",
+        ["Mother Teresa", "Margaret Thatcher", "Indira Gandhi"]
+      ],
+      [
+        "What was the name of the ship that Charles Darwin sailed on during his famous voyage?",
+        "HMS Beagle",
+        ["HMS Challenger", "HMS Bounty", "HMS Endeavour"]
+      ],
+      [
+        "Which two countries were involved in the Hundred Years' War?",
+        "England and France",
+        ["Spain and Portugal", "Germany and Austria", "Italy and Greece"]
+      ],
+]
+
+export const ScienceQuestions = [
+
+]
+
+export const SportsQuestions = [
+    
+]
